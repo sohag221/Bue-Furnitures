b9ac12623ee21b7846d25c2eef88f214f00dd9e5
sohag221 <mdratulsohag1234@gmail.com>
2024-07-15 12:39:00 +0600

    Pre-Requisites are added

---
+++ app/src/main/java/com/example/buyfurniture/activities/LoginRegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.example.buyfurniture.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.buyfurniture.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_register_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
